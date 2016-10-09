c8de5d148fe07a3b743e203a27a738f9f41b69d0
alexis <alexiscamus60@gmail.com>
2016-10-09 13:35:07 +0200

    Few fixes for more dynamic integration

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import $ from "jquery";
+window.$ = $;
+
+window.next = function () {
+    const $currentEvent = $(".event:not(.event-next,.event-past)");
+    const $nextEvent = $(".event-next").first();
+    $currentEvent.addClass("event-past");
+    $nextEvent.removeClass("event-next");
+};
